c1e40a9128ff0ce1b53f2f8964b4d347ffdc0153
abhaygill338 <abhaygill206@gmail.com>
2019-10-07 19:33:04 -0700

    Implementing upload function in Main Activity 3 , uploading all new sign tutorial videos in a video grid so that the user can practise all sign videos.

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -79,13 +79,45 @@ public class MainActivity2 extends AppCompatActivity {
             gesture_mapping.put("gift", "23/23781.mp4");
             gesture_mapping.put("car", "26/26165.mp4");
             gesture_mapping.put("pay", "14/14618.mp4");
+            gesture_mapping.put("pet", "25/25066.mp4");
+            gesture_mapping.put("sell", "9/9199.mp4");
+            gesture_mapping.put("explain", "22/22623.mp4");
+            gesture_mapping.put("that", "14/14366.mp4");
+            gesture_mapping.put("book", "14/14326.mp4");
+            gesture_mapping.put("now", "7/7774.mp4");
+            gesture_mapping.put("work", "14/14523.mp4");
+            gesture_mapping.put("total", "26/26467.mp4");
+            gesture_mapping.put("trip", "9/9117.mp4");
+            gesture_mapping.put("future", "14/14736.mp4");
+            gesture_mapping.put("good", "21/21534.mp4");
+            gesture_mapping.put("thank you", "21/21533.mp4");
+            gesture_mapping.put("learn", "21/21560.mp4");
+            gesture_mapping.put("agent", "10/10000.mp4");
+            gesture_mapping.put("should", "9/9563.mp4");
+            gesture_mapping.put("like", "6/6394.mp4");
+            gesture_mapping.put("movie", "8/8626.mp4");
+
             /*
             Gift - https://www.signingsavvy.com/media/mp4-ld/23/23781.mp4
             Car - https://www.signingsavvy.com/media/mp4-ld/26/26165.mp4
             Pay - https://www.signingsavvy.com/media/mp4-ld/14/14618.mp4
             Pet - https://www.signingsavvy.com/media/mp4-ld/25/25066.mp4
             Sell - https://www.signingsavvy.com/media/mp4-ld/9/9199.mp4
-            Explain - https://www.signingsavvy.com/media/mp4-ld/22/22623.mp4 That - https://www.signingsavvy.com/media/mp4-ld/14/14366.mp4 Book - https://www.signingsavvy.com/media/mp4-ld/14/14326.mp4 Now - https://www.signingsavvy.com/media/mp4-ld/7/7774.mp4 Work - https://www.signingsavvy.com/media/mp4-ld/14/14523.mp4 Total - https://www.signingsavvy.com/media/mp4-ld/26/26467.mp4 Trip - https://www.signingsavvy.com/media/mp4-ld/9/9117.mp4 Future - https://www.signingsavvy.com/media/mp4-ld/14/14736.mp4 Good - https://www.signingsavvy.com/media/mp4-ld/21/21534.mp4 Thank you - https://www.signingsavvy.com/media/mp4-ld/21/21533.mp4 Learn - https://www.signingsavvy.com/media/mp4-ld/21/21560.mp4 Agent - https://www.signingsavvy.com/media/mp4-ld/10/10000.mp4 Should - https://www.signingsavvy.com/media/mp4-ld/9/9563.mp4 Like - https://www.signingsavvy.com/media/mp4-ld/6/6394.mp4 Movie - https://www.signingsavvy.com/media/mp4-ld/8/8626.mp4
+            Explain - https://www.signingsavvy.com/media/mp4-ld/22/22623.mp4
+            That - https://www.signingsavvy.com/media/mp4-ld/14/14366.mp4
+            Book - https://www.signingsavvy.com/media/mp4-ld/14/14326.mp4
+            Now - https://www.signingsavvy.com/media/mp4-ld/7/7774.mp4
+            Work - https://www.signingsavvy.com/media/mp4-ld/14/14523.mp4
+            Total - https://www.signingsavvy.com/media/mp4-ld/26/26467.mp4
+            Trip - https://www.signingsavvy.com/media/mp4-ld/9/9117.mp4
+            Future - https://www.signingsavvy.com/media/mp4-ld/14/14736.mp4
+            Good - https://www.signingsavvy.com/media/mp4-ld/21/21534.mp4
+            Thank you - https://www.signingsavvy.com/media/mp4-ld/21/21533.mp4
+            Learn - https://www.signingsavvy.com/media/mp4-ld/21/21560.mp4
+            Agent - https://www.signingsavvy.com/media/mp4-ld/10/10000.mp4
+            Should - https://www.signingsavvy.com/media/mp4-ld/9/9563.mp4
+            Like - https://www.signingsavvy.com/media/mp4-ld/6/6394.mp4
+            Movie - https://www.signingsavvy.com/media/mp4-ld/8/8626.mp4
              */
             File SDCardRoot = Environment.getExternalStorageDirectory(); // location where you want to store
             File directory = new File(SDCardRoot, "/my_folder/"); //create directory to keep your downloaded file
--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -1,18 +1,31 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -28,14 +41,16 @@ public class MainActivity3 extends AppCompatActivity {
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                UploadTask up1 = new UploadTask();
+                Toast.makeText(getApplicationContext(),"Starting to Upload",Toast.LENGTH_LONG).show();
+                up1.execute();
                 Intent int2 = new Intent(MainActivity3.this, MainActivity.class);
                 startActivity(int2);
             }
         });
     }
 
-    public void startRecording()
-    {
+    public void startRecording() {
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
 
@@ -47,12 +62,135 @@ public class MainActivity3 extends AppCompatActivity {
                 + "/GESTURE_PRACTICE_" + pn + "_" + Global.lastName + ".mp4");
 
 
-
         Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT,5);
-            fileUri = Uri.fromFile(mediaFile);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 5);
+        fileUri = Uri.fromFile(mediaFile);
 
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
         startActivityForResult(intent, VIDEO_CAPTURE);
     }
+
+    public class UploadTask extends AsyncTask<String, String, String> {
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+
+                String url = "http://10.218.107.121/cse535/upload_video.php";
+                String charset = "UTF-8";
+                String group_id = "40";
+                String ASUid = "1200072576";
+                String accept = "1";
+
+
+                //File videoFile = new File(Environment.getExternalStorageDirectory()+"/my_folder/Action1.mp4");
+                File videoFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                        + "/GESTURE_PRACTICE_1_p.mp4");
+                String boundary = Long.toHexString(System.currentTimeMillis()); // Just generate some unique random value.
+                String CRLF = "\r\n"; // Line separator required by multipart/form-data.
+
+                URLConnection connection;
+
+                System.out.println("----------1");
+                connection = new URL(url).openConnection();
+
+                connection.setDoOutput(true);
+                System.out.println("----------2-----");
+                connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+                System.out.println("----------3-----");
+                OutputStream output1 = connection.getOutputStream();
+
+
+                System.out.println("In here");
+                try (
+                        OutputStream output = connection.getOutputStream();
+                        PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
+                ) {
+                    System.out.println("----------4-----");
+                    // Send normal accept.
+                    writer.append("--" + boundary).append(CRLF);
+                    writer.append("Content-Disposition: form-data; name=\"accept\"").append(CRLF);
+                    writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                    writer.append(CRLF).append(accept).append(CRLF).flush();
+
+                    // Send normal accept.
+                    writer.append("--" + boundary).append(CRLF);
+                    writer.append("Content-Disposition: form-data; name=\"id\"").append(CRLF);
+                    writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                    writer.append(CRLF).append(ASUid).append(CRLF).flush();
+
+                    // Send normal accept.
+                    writer.append("--" + boundary).append(CRLF);
+                    writer.append("Content-Disposition: form-data; name=\"group_id\"").append(CRLF);
+                    writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                    writer.append(CRLF).append(group_id).append(CRLF).flush();
+
+
+                    // Send video file.
+                    writer.append("--" + boundary).append(CRLF);
+                    writer.append("Content-Disposition: form-data; name=\"uploaded_file\"; filename=\"" + videoFile.getName() + "\"").append(CRLF);
+                    writer.append("Content-Type: video/mp4; charset=" + charset).append(CRLF); // Text file itself must be saved in this charset!
+                    writer.append(CRLF).flush();
+                    FileInputStream vf = new FileInputStream(videoFile);
+                    try {
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = 0;
+                        while ((bytesRead = vf.read(buffer, 0, buffer.length)) >= 0)
+                        {
+                            output.write(buffer, 0, bytesRead);
+
+                        }
+                        //   output.close();
+                        Toast.makeText(getApplicationContext(),"Upload Working", Toast.LENGTH_LONG).show();
+                    }
+
+                    catch (Exception exception)
+                    {
+
+
+                        Toast.makeText(getApplicationContext(),"output exception in catch....."+ exception + "", Toast.LENGTH_LONG).show();
+                        Log.d("Error", String.valueOf(exception));
+                        publishProgress(String.valueOf(exception));
+                        // output.close();
+
+                    }
+
+                    output.flush(); // Important before continuing with writer!
+                    writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+
+
+                    // End of multipart/form-data.
+                    writer.append("--" + boundary + "--").append(CRLF).flush();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e1) {
+                    System.out.println("In Catch Block!");
+                    e1.printStackTrace();
+                }
+
+                // Request is lazily fired whenever you need to obtain information about response.
+                int responseCode = ((HttpURLConnection) connection).getResponseCode();
+                System.out.println(responseCode); // Should be 200
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Exception!!!");
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+            Toast.makeText(getApplicationContext(), "In Background Task " + text[0], Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 }
